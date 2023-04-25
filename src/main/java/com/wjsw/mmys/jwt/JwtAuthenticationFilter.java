67096c10eacd851bcdedcc2415af56f51cb39336
SoonWookHwang <bravadosw@naver.com>
2023-04-25 22:59:02 +0900

    feat : JWT token provider, filter 추가

---
+++ src/main/java/com/wjsw/mmys/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.wjsw.mmys.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 헤더에서 JWT 를 받아온다.
+        log.info("jwtauthenticationFilter 진입");
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        log.info("리퀘스트에 담긴 토큰 = "+ token);
+        // 저장된 JWT 에서 "Bearer " 문자열 부분 제거하고 토큰값만 남김
+        if (StringUtils.hasText(token) && token.startsWith(BEARER_PREFIX)) {
+            token = token.substring(7);
+        }
+        // 유효한 토큰인지 확인한다.
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            log.info("토큰 유효성 검사 시작");
+            try {
+                // 토큰이 유효하면 토큰으로부터 유저 정보를 받아온다.
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                // SecurityContext 에 Authentication 객체를 저장한다.
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("Authentication 이름 ="+ authentication.getName());
+            } catch (IllegalArgumentException e) {
+                logger.error("an error occured during getting username from token", e);
+                throw new JwtException("유효하지 않은 토큰");
+            } catch (ExpiredJwtException e) {
+                logger.warn("the token is expired and not valid anymore", e);
+                throw new JwtException("토큰 기한 만료");
+            } catch (SignatureException e) {
+                logger.error("Authentication Failed. Username or Password not valid.");
+                throw new JwtException("사용자 인증 실패");
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/com/wjsw/mmys/jwt/JwtTokenProvider.java
@@ -0,0 +1,118 @@
+package com.wjsw.mmys.jwt;
+
+import com.wjsw.mmys.dto.TokenDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+
+// org.springframework.security.oauth2.jose.jws;
+//  io.jsonwebtoken;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    private static final String BEARER_TYPE = "Bearer ";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L*60*2; //5분
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L*60*60*3; //3시간
+
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.refresh-key}")
+    private String refreshKey;
+
+
+    private final UserDetailsService userDetailsService;
+
+    // 객체 초기화, secretKey 를 Base64로 인코딩한다.
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    /*
+    JWT Access 토큰 , Refresh 토큰 생성
+     */
+    public TokenDto createToken(String userPk/*,List<String> roles*/) {
+//        claims.put("roles", roles);
+        Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
+        Date now = new Date();
+        Date accessTokenExpiresIn = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
+        log.info("만료시간= " +accessTokenExpiresIn.getTime());
+        log.info("현재시간= " +now.getTime());
+
+        // Access Token 생성
+        String accessToken = Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now)
+                .setExpiration(accessTokenExpiresIn) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘과 signature 에 들어갈 secret 값 세팅
+                .compact();
+        log.info("토큰을 만들때 authentication객체의 이름= "+userPk);
+
+        // Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME))
+                .signWith(SignatureAlgorithm.HS256, refreshKey)
+                .compact();
+
+        return TokenDto.builder()
+                .grantType(BEARER_TYPE)
+                .accessToken(accessToken)
+                .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
+                .refreshToken(refreshToken)
+                .build();
+    }
+    public Authentication getAuthentication(String token) {
+        log.info("authentication을 가져올때 들어가는 이름 ="+this.getUserPk(token));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserPk(String token) {
+        log.info("유저 pk(이름)="+Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+    public String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        log.info("ResolveToken 쿠키에 담긴 토큰 ="+ token);
+        return token;
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            log.info("accesss token claims정보"+ claims);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public boolean validateRefreshToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(refreshKey).parseClaimsJws(jwtToken);
+            log.info("refresh token claims정보"+ claims);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
